248d5ec13b9a28d0eace0da60244539b2952493e
tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
2025-03-26 10:26:01 +0530

    Event Date Changes
    1. Backend should handle the date for processing the event types.

--- course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/event/impl/EventEnrolmentDaoImpl.java
+++ course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/event/impl/EventEnrolmentDaoImpl.java
@@ -434,11 +434,11 @@ public class EventEnrolmentDaoImpl implements EventEnrolmentDao {
         Map<String, Object> contentDetails =
             getEventDetails(request.getRequestContext(), contentId);
         String actualEventType = null;
-        String endDateStr = (String) request.get("eventEndDate");
         boolean calendarEventEnabled = (boolean) request.get("calendarEventEnabled");
         LocalTime endTime = LocalTime.of(0, 1);
         if (MapUtils.isNotEmpty(contentDetails)) {
           if (calendarEventEnabled) {
+            String endDateStr = (String) request.get("eventEndDate");
             processCalendarEvent(
                 request,
                 enrollment,
@@ -451,7 +451,7 @@ public class EventEnrolmentDaoImpl implements EventEnrolmentDao {
                 requestedEventType,
                 endDateStr);
           } else {
-            actualEventType = determineEventType(endDateStr, endTime, contentDetails);
+            actualEventType = determineEventType(endTime, contentDetails);
             addEventDetailsToEnrollment(
                 request,
                 enrollment,
@@ -503,17 +503,16 @@ public class EventEnrolmentDaoImpl implements EventEnrolmentDao {
     }
   }
 
-  private String determineEventType(
-      String endDateStr, LocalTime endTime, Map<String, Object> contentDetails) {
-    LocalDate endDate = LocalDate.parse(endDateStr);
+  private String determineEventType(LocalTime endTime, Map<String, Object> contentDetails) {
+    LocalDate currentDate = LocalDate.now();
     LocalDate eventEndDate = LocalDate.parse(contentDetails.get("endDate").toString());
     OffsetTime eventEndTime = OffsetTime.parse(contentDetails.get("endTime").toString());
     LocalDate eventStartDate = LocalDate.parse(contentDetails.get("startDate").toString());
-    if (eventEndDate.isBefore(endDate)
-        || (eventEndDate.isEqual(endDate)
+    if (eventEndDate.isBefore(currentDate)
+        || (eventEndDate.isEqual(currentDate)
             && eventEndTime.isBefore(OffsetTime.of(endTime, ZoneOffset.ofHoursMinutes(5, 30))))) {
       return "pastEvent";
-    } else if (eventStartDate.isEqual(endDate)) {
+    } else if (eventStartDate.isEqual(currentDate)) {
       return "presentEvent";
     } else {
       return "futureEvent";
